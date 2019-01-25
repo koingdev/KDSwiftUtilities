31622f1e98fc549cdcb5c3d5ebe6f9e0f8fecd8b
KoingDev <sea.saingkoing@gmail.com>
2019-01-25 14:21:37 +0700

    Init project

---
+++ KDSwiftUtilities/Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KDSwiftUtilities
+//
+//  Created by KoingDev on 2019/01/23.
+//  Copyright © 2019 KoingDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+inhibit_all_warnings!
+platform :ios, '9.0'
+
+target 'KDSwiftUtilities' do
+
+  use_frameworks!
+	
+	pod 'ReachabilitySwift'
+
+end
