cc4bd97bc9fff7a7efec05291828b8f9c3b9351b
KoingDev <sea.saingkoing@gmail.com>
2019-02-01 17:11:18 +0700

    Update UIAlertController Builder (Progress)

--- Extensions/AlertController.swift
+++ Extensions/AlertController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-public extension UIAlertController {
+extension UIAlertController {
     
     // MARK: - Typealias
     
@@ -33,44 +33,60 @@ public extension UIAlertController {
                                       then: (([String]) -> Void)? = nil) {
         let builder = UIAlertController.Builder()
         builder.withTitleAndMessage(title: title, message: message)
-            .addOkCancelAction(okayHandler: { _ in
-                guard let then = then else { return }
-                var texts = [String]()
-                builder.alert.textFields?.forEach { texts.append($0.text ?? "") }
-                then(texts)
-                builder.alert = nil    // Avoid retain cycle
-            }, cancelHandler: { _ in builder.alert = nil })
-            .addTextField(numberOfField: numberOfField, placeholders: placeholders, isSecureTexts: isSecureTexts)
-            .show()
+			.addOkCancelAction(okayHandler: { _ in
+				guard let then = then else { return }
+				var texts = [String]()
+				builder.alert.textFields?.forEach { texts.append($0.text ?? "") }
+				then(texts)
+				builder.alert = nil    // Avoid retain cycle
+			}, cancelHandler: { _ in builder.alert = nil })
+			.addTextField(numberOfField: numberOfField, placeholders: placeholders, isSecureTexts: isSecureTexts)
+			.show()
     }
     
     // MARK: - Builder
     
-    public class Builder {
+	class Builder {
         
-        fileprivate var alert: Alert! = Alert(title: "", message: "", preferredStyle: .alert)
-        
+        private var alert: Alert! = Alert(title: "", message: "", preferredStyle: .alert)
+		
+		enum ActionType: CaseIterable {
+			case ok
+			case cancel
+			case custom
+		}
+		
         @discardableResult
-        public func withTitle(_ title: String = "Title") -> Self {
-            alert.title = title
+		func withTitleAndMessage(title: String = "Title", message: Any = "Message") -> Self {
+			alert.title = title
+			if let message = message as? String {
+				alert.message = message
+			}
+			if let attributedMessage = message as? NSAttributedString {
+				alert.setValue(attributedMessage, forKey: "attributedMessage")
+			}
             return self
         }
-        
-        @discardableResult
-        public func withMessage(_ message: Any = "Message") -> Self {
-            if let message = message as? String {
-                alert.message = message
-            }
-            if let attributedMessage = message as? NSAttributedString {
-                alert.setValue(attributedMessage, forKey: "attributedMessage")
-            }
-            return self
-        }
-        
-        @discardableResult
-        public func withTitleAndMessage(title: String = "Title", message: Any = "Message") -> Self {
-            return withTitle(title).withMessage(message)
-        }
+		
+		func addActions(_ actionTypes: (ActionType, ActionType), completion: @escaping () -> Void) -> Self {
+			// Aux functions
+			func setAction(title: String, style: UIAlertAction.Style = .default, completion: @escaping () -> Void) {
+				let action = Action(title: title, style: .cancel) { _ in
+					completion()
+				}
+				alert.addAction(action)
+			}
+			func apply(actionType: ActionType) {
+				switch actionType {
+				case .ok:
+					setAction(title: "Okay", completion: completion)
+				case .cancel:
+					setAction(title: "Cancel", style: .cancel, completion: completion)
+				case .custom:
+					
+				}
+			}
+		}
         
         @discardableResult
         public func addOkAction(_ handler: Handler = nil) -> Self {
