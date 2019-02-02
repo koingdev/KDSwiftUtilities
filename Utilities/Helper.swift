217126cee8fe8928a0fddc39caf153fc5239e4b0
KoingDev <sea.saingkoing@gmail.com>
2019-02-02 22:45:31 +0700

    Helper method check isUserAllowPushNotification added

--- Utilities/Helper.swift
+++ Utilities/Helper.swift
@@ -7,6 +7,14 @@
 //
 
 import Foundation
+import UIKit.UIApplication
+import UserNotifications
+
+////////////////////////////////////////////////////////////////
+//MARK: -
+//MARK: Helper Methods
+//MARK: -
+////////////////////////////////////////////////////////////////
 
 func dPrint(_ item: Any) {
 	#if DEBUG
@@ -14,3 +22,31 @@ func dPrint(_ item: Any) {
 	#endif
 }
 
+/// Check if user allows push notification or not
+///
+/// - Parameter completion: Result callback (Background Thread).
+///                            Make sure to do UI-related tasks on Main Thread.
+func isUserAllowPushNotification(completion: @escaping (Bool) -> Void) {
+    if #available(iOS 10.0, *) {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            let isUserAllow: Bool
+            switch settings.authorizationStatus {
+            case .notDetermined, .denied:
+                isUserAllow = false
+            case .authorized:
+                isUserAllow = true
+            default:
+                isUserAllow = false
+            }
+            completion(isUserAllow)
+        }
+    } else {
+        // Fallback on earlier versions
+        if let notificationType = UIApplication.shared.currentUserNotificationSettings?.types {
+            let isUserAllow = !notificationType.isEmpty
+            completion(isUserAllow)
+        } else {
+            completion(false)
+        }
+    }
+}
